7da0df14c7ced216dabdb1b911d972cf98e6ebb6
Tmothy Perfitt <tperfitt@twocanoes.com>
2026-04-20 18:53:17 -0500

    add post build script

--- build_ios.sh
+++ build_ios.sh
@@ -44,20 +44,6 @@ xcodebuild -project "${PROJECT_NAME}.xcodeproj" -scheme "${SCHEME}" -configurati
 
 xcodebuild -exportArchive -archivePath "${build_folder}/${PROJECT_NAME}.xcarchive" -exportOptionsPlist exportOptions.plist -exportPath "${build_folder}" -allowProvisioningUpdates 
 
-zip --symlinks --recurse-paths "${IPA}" .
-#./package_ipa.sh "${build_folder}/${PROJECT_NAME}.xcarchive/Products/Applications/Smart Card Utility.app" "${build_folder}/${PROJECT_NAME}.xcarchive/Products/${APP_NAME}.ipa"
 
-#pushd "${build_folder}/"
-#mkdir tmp
-#mv "${APP_NAME}.ipa" "tmp/${APP_NAME}.zip"
-#cd tmp
-#unzip "${APP_NAME}.zip"
-#rm "${APP_NAME}.zip"
-#cp -Rvp "${build_folder}/${PROJECT_NAME}.xcarchive"/SwiftSupport .
-#zip --symlinks --verbose --recurse-paths "${build_folder}/${PROJECT_NAME}.xcarchive/Products/${APP_NAME}.ipa" .
-#cd ..
-
-#echo "uploading...."
-#xcrun altool --upload-app --type ios --file "${build_folder}/${PROJECT_NAME}.xcarchive/Products/${APP_NAME}.ipa"  -u "${app_store_id}" -p "${app_store_password}"
-
+cp "${build_folder}"/"${APP_NAME}.ipa" /tmp/
 echo "build ${build_number} finished"
