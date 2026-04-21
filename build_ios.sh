6f1813ab85cd369a38b591d2f5b2a4ddccaee0cc
Tmothy Perfitt <tperfitt@twocanoes.com>
2026-04-20 19:03:29 -0500

    add post build script

--- build_ios.sh
+++ build_ios.sh
@@ -45,7 +45,7 @@ xcodebuild -project "${PROJECT_NAME}.xcodeproj" -scheme "${SCHEME}" -configurati
 xcodebuild -exportArchive -archivePath "${build_folder}/${PROJECT_NAME}.xcarchive" -exportOptionsPlist exportOptions.plist -exportPath "${build_folder}" -allowProvisioningUpdates 
 
 GH_TOKEN=${github_api_token}
-
+. ../build/github_creds.sh
 gh release create "${version}.${build_number}" --generate-notes "${build_folder}"/"${APP_NAME}.ipa"
 
 echo "build ${build_number} finished"
